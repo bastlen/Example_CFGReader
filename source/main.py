05ca4c9c57a5fdf3c1329d53b20e6d74125766d1
Basteln <bast.eln@web.de>
2019-03-18 15:40:27 +0100

    structure2

--- source/main.py
+++ source/main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import configparser as cp
+#TODO
+#from source import pcl.PCLHandler as pclh
+
+def main():
+    # read cfg from ini
+
+    # do stuff with pcl
+    #TODO
+    #pcl1 = pclh()
+    print("main")
+
+
+if __name__ == "__main__":
+    main()
---
+++ source/pcl/PCLHandler.py
@@ -0,0 +1,2 @@
+class PCLHandler:
+    """ PCLHandler handles file i/o of .xyz files and stores them in numpy."""
