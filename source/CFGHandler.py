318187e7e183b6b3be0992bb846d20bed3686cb0
Basteln <bast.eln@web.de>
2019-03-18 17:01:41 +0100

    structure2

--- source/CFGHandler.py
+++ source/CFGHandler.py
@@ -2,3 +2,12 @@ import configparser as cp
 
 class CFGHandler:
     """ Read INI-Files"""
+
+def __init__(self,cfg_name):
+    self.cfg_name = cfg_name
+
+def getCFG(path):
+    try:
+        return cp
+    except:
+        print("Error File I/O!")
